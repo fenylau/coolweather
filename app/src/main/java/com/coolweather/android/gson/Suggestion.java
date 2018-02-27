5ab9c404d7f0e5e94e66d6c21d74cd2faecf102b
fenylau <18951539055@189.cn>
2018-02-27 21:09:08 +0800

    加入显示天气的功能。

--- app/src/main/java/com/coolweather/android/gson/Suggestion.java
+++ app/src/main/java/com/coolweather/android/gson/Suggestion.java
@@ -9,7 +9,7 @@ import com.google.gson.annotations.SerializedName;
 public class Suggestion {
     @SerializedName("comf")
     public Comfort comfort;
-    @SerializedName("CW")
+    @SerializedName("cw")
     public CarWash carWash;
     public Sport sport;
     public class Comfort{
